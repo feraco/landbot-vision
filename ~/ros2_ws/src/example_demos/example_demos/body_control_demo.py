d2b26ae287a1c94da92baddfc2e9af554dd5f811
Fred Feraco <frederick.feraco@gmail.com>
2025-06-18 13:42:52 -0400

    Fix OpenCV Window Display in Demos

--- ~/ros2_ws/src/example_demos/example_demos/body_control_demo.py
+++ ~/ros2_ws/src/example_demos/example_demos/body_control_demo.py
@@ -16,7 +16,6 @@ from rclpy.node import Node
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
-from std_msgs.msg import String
 
 class BodyControlDemo(Node):
     def __init__(self):
